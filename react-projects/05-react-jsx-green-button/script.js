043d708bd5d2ca52f8ddd03226d107c3f7467fcb
Shade78 <43784874+Shade78@users.noreply.github.com>
2023-03-22 21:28:31 +0300

    Теперь поведение такое же как и в 01-vanilla-js

--- react-projects/05-react-jsx-green-button/script.js
+++ react-projects/05-react-jsx-green-button/script.js
@@ -1,15 +1,16 @@
-const App = () => {
+const App = ({ initialButtonText, buttonClass }) => { // деструктуризация объекта
     // функциональный компонент
     // чтобы измнения при нажатии на кнопку перенеслись, нужно вызвать фунцию еще раз
-    const [buttonText, setButtonText] = React.useState('Click me');
-    console.log(buttonText);
+    const [buttonText, setButtonText] = React.useState(initialButtonText);  
+    const [classesList, setClassesList] = React.useState(buttonClass)  // buttonText и classesList -    состояние
 
     const onButtonclick = () => {
         setButtonText('button has been pressed');
+        setClassesList('green-btn');
     };
     return (
         <div className="app">
-            <button onClick={onButtonclick}>{buttonText}</button>
+            <button className={classesList} onClick={onButtonclick}>{buttonText}</button>
             {/*{любое выражение JS} */}
         </div>
     );
@@ -17,5 +18,4 @@ const App = () => {
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
-
+root.render(<App initialButtonText="click me 123" buttonClass="someClassName"/>);  // свойства
